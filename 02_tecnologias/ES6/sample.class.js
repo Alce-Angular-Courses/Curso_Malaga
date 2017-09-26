0cfa03e0b6d6a96dcf26c8825794b636712850d2
alce65 <alce65@hotmail.es>
2017-09-26 09:51:06 +0200

    Cambio fichero clases

--- 02_tecnologias/ES6/sample.class.js
+++ 02_tecnologias/ES6/sample.class.js
@@ -1,18 +1,31 @@
 // Ejemplo de código en ES6
 
-class Libro {}
-
-class LibroTecnico extends Libro {  
+class Libro {
     constructor(tematica, paginas) {
-        super(tematica, paginas);
-        this.capitulos = [];
-        this.precio = "";
-        // ...
-    }
-    
-    metodo(pValor = "foo") {
-        // ...
+        this.tematica = tematica  
+        this.paginas = paginas
     }
 }
+
+class LibroTecnico extends Libro {
+
+	constructor(tematica, paginas, precio) {
+		super(tematica, paginas);
+		this.capitulos = [];
+		this.precio = precio;
+		// ...
+	}
+	
+	precioFinal(pIva = 16) {
+		return this.precio * (1 + pIva/100)
+	}
+}
+
+let libro1 = new LibroTecnico("Informatica", 250, 30)
+console.dir(libro1)
+console.dir(libro1.precioFinal())
+console.dir("En Canarias : ")
+console.dir(libro1.precioFinal(0))
+
 
 
